e58d82baeb29b27e445426819e54f4d5f9d1f8c9
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-21 22:55:45 +0530

    complete hero sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import bgVideo from "./assets/earth.mp4"
 import Nav from "./components/Nav"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className='h-[700px] relative'>
         <video autoPlay loop muted src={bgVideo}className='fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]'></video>
         <Nav/>
+        <Hero/>
       </div>
     </div>
     </>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className='bg-black/20 h-full relative z-50'>
+        <div className="h-full flex justify-center items-center p-4 ">
+            <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+                <div>
+                    <h1 className='text-5xl font-bold'>
+                        Welcome to MAHAKASH
+                    </h1>
+                    <p className='mt-10'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem repudiandae consequuntur pariatur officia suscipit debitis consequatur quos iure maiores natus? Lorem ipsum dolor sit, amet consectetur adipisicing elit. At minima blanditiis sit obcaecati non nostrum?
+                     </p>
+                     <button className='bg-white text-black px-4 py-2 rounded-md mt-5 hover:bg-slate-300'>
+                        Explore
+                     </button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Hero
