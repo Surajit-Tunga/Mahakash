d09beb4d84f1d88d697d91bededc2d238fc27c15
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-22 12:14:47 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,12 +27,17 @@ const About = () => {
             <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 relative z-10'>
                 { 
                     AboutData.map((data, index) => (
-                        <div key={index}>
-                            <h1>{data.title}</h1>
+                        <div key={index} className='min-h-[180px] flex flex-col justify-center items-center bg-sky-900/70 rounded-xl backdrop-blur-sm text-center text-2xl py-8 px-3 w-full lg:w-[300px] mx-auto'>
+                            {data.icon}
+                            <h1 className='my-3'>{data.title}</h1>
+                            <p className='text-sm'>{data.desc}</p>
                         </div>
                     ))
                 }
             </div>
+            {/* <img src={wave} alt="" 
+            classname="h-[200px] w-full object-cover mix-blend-screen-translate-y-24 relative z-[0]"
+            /> */}
         </div>
       </div>
     </div>
